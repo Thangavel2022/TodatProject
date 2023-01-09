f2f6c0e770c9f2e931192778bd15e97914d6f041
ThangavelSugu1 <subramanispb9@gmail.com>
2023-01-09 11:11:40 +0530

    added to new

--- src/org/test/Sample.java
+++ src/org/test/Sample.java
@@ -1,11 +1,18 @@
 package org.test;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Sample {
 public static void main(String[] args) {
 
 	System.out.println("suguna");
 
 	System.out.println("thangavel");
+	System.setProperty("webdriver.chrome.driver", "E:\\greens\\chromeDrive\\chromedriver.exe");
+	WebDriver driver=new ChromeDriver();
+	driver.get("https://www.facebook.com/");
+	
 
 }
 }
